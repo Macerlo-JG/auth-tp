c9a9dee29155bdaaa49be8a16262723f28794e0f
shoebillstorkyeah <verondylanban@gmail.com>
2026-06-25 20:23:02 -0300

    mainapp

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,30 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import ListadoUsuarios from "./pages/ListadoUsuarios"
-import VerUsuario from "./pages/VerUsuario"
-import NuevoUsuario from "./pages/NuevoUsuario"
-import EditarUsuarioPage from "./pages/EditarUsuarioPage"
-import './App.css'
+import { Toaster } from "react-hot-toast";
+import { Routes, Route, Navigate } from "react-router-dom";
+import "./index.css";
+import ListadoUsuarios from "./paginas/ListadoUsuarios.jsx";
+import NuevoUsuario from "./paginas/NuevoUsuario.jsx";
+import EditarUsuarioPage from "./paginas/EditarUsuarioPage.jsx";
+import VerUsuario from "./paginas/VerUsuario.jsx";
 
 function App() {
-
   return (
+    <>
+      <Toaster
+        position="top-right"
+        toastOptions={{
+          duration: 2500,
+          style: { zIndex: 9999 },
+        }}
+      />
       <Routes>
-        <Route path="/" element={<ListadoUsuarios/>}/>
-        <Route path="/editar" element={<EditarUsuarioPage/>}/>
-        <Route path="/nuevousuario" element={<ListadoUsuarios/>}/>
-        <Route path="/verusuario" element={<ListadoUsuarios/>}/>
+        <Route path="/" element={<Navigate to="/usuarios" replace />} />
+        <Route path="/usuarios" element={<ListadoUsuarios />} />
+        <Route path="/usuarios/nuevo" element={<NuevoUsuario />} />
+        <Route path="/usuarios/:id" element={<VerUsuario />} />
+        <Route path="/usuarios/:id/editar" element={<EditarUsuarioPage />} />
       </Routes>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+//Importa StricMode que sirve para revisar errores en React
+import { StrictMode } from "react";
 
-createRoot(document.getElementById('root')).render(
+//Conecta React con la pagina Web
+import { createRoot } from "react-dom/client";
+
+//Sirve para cambiar de pagina sin recargar todo el sitio 
+import { BrowserRouter } from "react-router-dom";
+
+//Traemos los estilos CSS
+import "./index.css";
+
+//Traemos la parte principal
+import App from "./App.jsx";
+
+//buscamos la parte de la pagina web donde esta root y ahi vemos toda la app
+// es como capas sobre la app(donde va todo) va la capa de Browser Router 
+//  que permite navegar por distintas partes sin recargar las paginas y
+//Strict Mode sirve revisar errores en React
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-        <App />
+      <App />
     </BrowserRouter>
   </StrictMode>,
-)
+);
