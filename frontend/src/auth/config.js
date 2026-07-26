9c49b30fdd12ecbdcf73c00306b6402e2bb887a9
Marcelo-JG <marcelojdev@gmail.com>
2026-07-25 21:28:27 -0300

    esto no funciona en local, para local cambiar frontend/src/auth/config y a AUTH_API= = "http://Localhost:5000"

--- frontend/src/auth/config.js
+++ frontend/src/auth/config.js
@@ -1,4 +1,4 @@
-export const AUTH_API = "http://localhost:5000";
+export const AUTH_API = "http://172.99.0.10:5000";
 export const HOME_ROUTE = "/usuarios";
 export const LOGIN_ROUTE = "/login";
 export const STORAGE_KEY = "auth";
