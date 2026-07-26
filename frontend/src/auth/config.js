1dcf6fd2ce55bc5a036d47955684c1a00d997198
shoebillstorkyeah <verondylanban@gmail.com>
2026-07-26 16:05:35 -0300

    test 1

--- frontend/src/auth/config.js
+++ frontend/src/auth/config.js
@@ -1,4 +1,4 @@
-export const AUTH_API = "http://172.99.0.10:5924";
+export const AUTH_API = "http://192.168.0.164:8480";
 export const HOME_ROUTE = "/usuarios";
 export const LOGIN_ROUTE = "/login";
 export const STORAGE_KEY = "auth";
